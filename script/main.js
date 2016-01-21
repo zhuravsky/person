97ba8627e35259b5551ecd0dbce9008415bf314e
zhuravsky <unfounddoor1919@gmail.com>
2016-01-21 17:23:23 +0300

    вернула блок на главную в мобильной версии

--- script/main.js
+++ script/main.js
@@ -34,27 +34,27 @@
         });
     });
 
-    $(document).ready(function() {
-        $(".animsition").animsition({
-            inClass: 'fade-in',
-            outClass: 'fade-out',
-            inDuration: 500,
-            outDuration: 500,
-            linkElement: 'a:not([target="_blank"]):not([href^=#])',
-            loading: true,
-            loadingParentElement: 'body',
-            loadingClass: 'animsition-loading',
-            loadingInner: '',
-            timeout: false,
-            timeoutCountdown: 5000,
-            onLoadEvent: true,
-            browser: [ 'animation-duration', '-webkit-animation-duration'],
-            overlay : false,
-            overlayClass : 'animsition-overlay-slide',
-            overlayParentElement : 'body',
-            transition: function(url){ window.location.href = url; }
-        });
-    });
+    //$(document).ready(function() {
+    //    $(".animsition").animsition({
+    //        inClass: 'fade-in',
+    //        outClass: 'fade-out',
+    //        inDuration: 500,
+    //        outDuration: 500,
+    //        linkElement: 'a:not([target="_blank"]):not([href^=#])',
+    //        loading: true,
+    //        loadingParentElement: 'body',
+    //        loadingClass: 'animsition-loading',
+    //        loadingInner: '',
+    //        timeout: false,
+    //        timeoutCountdown: 5000,
+    //        onLoadEvent: true,
+    //        browser: [ 'animation-duration', '-webkit-animation-duration'],
+    //        overlay : false,
+    //        overlayClass : 'animsition-overlay-slide',
+    //        overlayParentElement : 'body',
+    //        transition: function(url){ window.location.href = url; }
+    //    });
+    //});
 
   $(document).ready(function(){
     if (window.outerWidth < 720) {
